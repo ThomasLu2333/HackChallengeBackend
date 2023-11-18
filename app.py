4f0ac8025593a9b95fc05cfce4578b6c2768af57
Thomas Lu <celu@harrowbeijing.cn>
2023-11-18 06:29:33 -0500

    Added template code to project

---
+++ app.py
@@ -0,0 +1,10 @@
+import json
+from flask import Flask, request
+import db
+
+DB = db.DatabaseDriver()
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
---
+++ db.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+# From: https://goo.gl/YzypOI
+def singleton(cls):
+    instances = {}
+
+    def getinstance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+
+    return getinstance
+
+
+class DatabaseDriver(object):
+    """
+    Database driver for the Task app.
+    Handles with reading and writing data with the database.
+    """
+
+    def __init__(self):
+        """
+        Secures a connection with the database and stores it into the
+        instance variable `conn`
+        """
+        self.conn = sqlite3.connect("todo.db", check_same_thread=False)
+        self.create_task_table()
+
+    # -- TASKS -----------------------------------------------------------
+
+    def create_task_table(self):
+        pass
+
+
+# Only <=1 instance of the database driver
+# exists within the app at all times
+DatabaseDriver = singleton(DatabaseDriver)
